fde9e1fb6d107ca1a9a16ed2354f92f32cd53120
Rabiul hossain <mohammadrabiul4@gmail.com>
2021-03-24 13:08:08 -0700

    syncing review cart and shop cart

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,40 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import fakeData from '../../fakeData';
-import { getDatabaseCart } from '../../utilities/databaseManager';
-import ReviewItem from '../ReviewItem/ReviewItem';
+import React, { useEffect, useState } from "react";
+import fakeData from "../../fakeData";
+import {
+  getDatabaseCart,
+  removeFromDatabaseCart,
+} from "../../utilities/databaseManager";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Review = () => {
-    const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
+  const removeProduct = (productKey) => {
+    console.log("remove clicked");
+    const newCart = cart.filter((pd) => pd.key !== productKey);
+    setCart(newCart);
+    removeFromDatabaseCart(productKey);
+  };
 
-    useEffect(() => {
-        //cart
-        const savedCart = getDatabaseCart();
-        const productKeys = Object.keys(savedCart);
-        const cartProducts = productKeys.map(key => {
-            const product = fakeData.find(pd => pd.key === key);
-            product.quantity = savedCart[key];
-            return product;
-        });
-        setCart(cartProducts);
-    }, []);
-    const reviewItemStyle = {
-        borderBottom: '1px solid lightgray',
-        marginBottom: '5px',
-        paddingBottom: '5px',
-        marginLeft: '200px'
-    }
-    return (
-        <div style={reviewItemStyle}className="review-item">
-           <h1>Cart Items: {cart.length}</h1> 
-           {
-               cart.map(pd => <ReviewItem 
-                key={pd.key}
-                product={pd}></ReviewItem>)
-           }
-           <br/>
-           <button className="main-btn">Remove Item</button>
-        </div>
-    );
+  useEffect(() => {
+    //cart
+    const savedCart = getDatabaseCart();
+    const productKeys = Object.keys(savedCart);
+    const cartProducts = productKeys.map((key) => {
+      const product = fakeData.find((pd) => pd.key === key);
+      product.quantity = savedCart[key];
+      return product;
+    });
+    setCart(cartProducts);
+  }, []);
+  const reviewItemStyle = {
+    borderBottom: "1px solid lightgray",
+    marginBottom: "5px",
+    paddingBottom: "5px",
+    marginLeft: "200px",
+  };
+  return (
+    <div style={reviewItemStyle} className="twin-container">
+      <div className="product-container">
+        {cart.map((pd) => (
+          <ReviewItem
+            removeProduct={removeProduct}
+            key={pd.key}
+            product={pd}
+          ></ReviewItem>
+        ))}
+      </div>
+      <div className="cart-conatiner">
+            <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Review;
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const ReviewItem = (props) => {
-    const {name, quantity} = props.product;
+    const {name, quantity, key, price} = props.product;
     return (
         <div>
             <h4 className="product-name">{name}</h4>
             <p>Quantity: {quantity}</p>
+            <p><small>${price}</small></p>
+            <br/>
+            <button 
+            onClick={() => props.removeProduct(key)}
+            className="main-btn">Remove Item</button>
         </div>
     );
 };
