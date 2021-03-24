5b6f735fdcbae19dde95ffeb1d0557fe34ec6b26
Rabiul hossain <mohammadrabiul4@gmail.com>
2021-03-24 06:02:53 -0700

    adding react router

---
+++ src/components/Review/Review.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import fakeData from '../../fakeData';
+import { getDatabaseCart } from '../../utilities/databaseManager';
+import ReviewItem from '../ReviewItem/ReviewItem';
+
+const Review = () => {
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        //cart
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+        const cartProducts = productKeys.map(key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        });
+        setCart(cartProducts);
+    }, []);
+    const reviewItemStyle = {
+        borderBottom: '1px solid lightgray',
+        marginBottom: '5px',
+        paddingBottom: '5px',
+        marginLeft: '200px'
+    }
+    return (
+        <div style={reviewItemStyle}className="review-item">
+           <h1>Cart Items: {cart.length}</h1> 
+           {
+               cart.map(pd => <ReviewItem 
+                key={pd.key}
+                product={pd}></ReviewItem>)
+           }
+           <br/>
+           <button className="main-btn">Remove Item</button>
+        </div>
+    );
+};
+
+export default Review;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,6 +3,7 @@ import fakeData from '../../fakeData';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
+import {addToDatabaseCart} from '../../utilities/databaseManager';
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
@@ -12,6 +13,9 @@ const Shop = () => {
     const handleAddProduct = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        const sameProduct = newCart.filter(pd => pd.key === product.key);
+        const count = sameProduct.length;
+        addToDatabaseCart(product.key, count);
     }
     console.log(fakeData);
     return (
@@ -20,6 +24,8 @@ const Shop = () => {
                 
                 {
                     products.map(pd => <Product
+                        key={pd.key}
+                        showAddToCart={true}
                         handleAddProduct = {handleAddProduct}
                         product={pd}></Product>)
                 }
